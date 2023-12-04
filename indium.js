cc5fee1330c36539d65a4b10b298032e3bf53d5b
tygzy <mail@tygzy.net>
2023-12-04 21:13:56 +0000

    Gallery added

--- indium.js
+++ indium.js
@@ -1,5 +1,5 @@
 /*
-* CSS must include a `.active` class for the pop-up window which has `display` to anything other than `none`.
+* CSS must include a `.active` class for the pop-up and dropdown which has `display` to anything other than `none`.
 * Can reference the various elements in html pages with needing to use `document.getElementById` etc
 * instead use the class variable referencing the element.
 *
@@ -81,5 +81,71 @@ class Dropdown {
                 this.close();
             }
         });
+    }
+}
+
+
+class Gallery {
+    /*
+    *
+    * @param {Element} container - The container of all the items that can be opened in the gallery.
+    * @param {Element} item_view - The container element which will be used to show a specific item onto the screen.
+    *
+    */
+
+    constructor(container, item_view) {
+        this.container = container;
+        this.item_view = item_view;
+        this.current_item = null;
+    }
+
+    get has_item_open() {
+        return !!this.current_item;
+    }
+
+    get get_current_item() {
+        return this.container.children[this.current_item];
+    }
+
+    next_item() {
+        if(this.current_item <= 1) {
+            this.current_item = 1;
+        } else if(this.current_item > this.container.children.length) {
+            this.current_item = this.container.children.length;
+        } else {
+            this.current_item += 1;
+        }
+    }
+
+    previous_item() {
+        if(this.current_item <= 1) {
+            this.current_item = 1;
+        } else if(this.current_item > this.container.children.length) {
+            this.current_item = this.container.children.length;
+        } else {
+            this.current_item -= 1;
+        }
+    }
+
+    open_item(item_number) {
+        this.open_gallery();
+        let new_item = document.createElement('img');
+        new_item.src = this.container.children[item_number].src;
+        this.current_item = item_number;
+
+        this.item_view.replaceChildren();
+        this.item_view.appendChild(new_item);
+    }
+
+    remove_item() {
+        this.item_view.replaceChildren();
+    }
+
+    close_gallery() {
+        this.item_view.classList.remove('active');
+    }
+
+    open_gallery() {
+        this.item_view.classList.add('active');
     }
 }
